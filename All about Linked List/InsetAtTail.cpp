fdcf56664b164b4a176eed5599a73c9bf52e1d71
farzanapomy <farzanapomy56@gmail.com>
2022-11-14 01:24:18 +0600

    first commit

---
+++ All about Linked List/InsetAtTail.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// to store value and node
+
+class Node
+{
+public:
+    int value;
+    Node *next;
+
+    // creating a constructor
+    Node(int val)
+    {
+        value = val;
+        next = NULL;
+    }
+};
+
+void insetAtTail(Node *&head, int value)
+{
+    Node *newNode = new Node(value);
+
+    // if head null then this will be work
+    if (head == NULL)
+    {
+        head = newNode;
+        return;
+    }
+    // if head!= NULL then this will be work.
+    Node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+
+void display(Node *n)
+{
+    while (n != NULL)
+    {
+        cout << n->value;
+        if (n->next != NULL)
+        {
+            cout << " " << endl;
+        }
+
+        n = n->next;
+    }
+}
+
+int main()
+{
+    Node *head = NULL;
+    cout << "Enter the value:";
+    int n;
+    cin >> n;
+    char choice = 'Y';
+
+    while (choice == 'Y')
+    {
+        insetAtTail(head, n);
+        cout << "Do you want to continue: (Y/N)";
+        cin >> choice;
+    }
+
+    // Node *second = new Node();
+    // Node *third = new Node();
+    // Node *fourth = new Node();
+
+    display(head);
+}
