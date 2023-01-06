d0232a7f0ae1852758d98eec6c3f26d827397b92
farzanapomy <farzanapomy56@gmail.com>
2023-01-06 16:03:23 +0600

    setup-stack

---
+++ All about Linked List/stack/stack.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// create  class
+
+class Node
+{
+public:
+    int value;
+    Node *next;
+    Node *pre;
+
+    Node(int value)
+    {
+        value = value;
+        next = NULL;
+        pre = NULL;
+    }
+};
+
+class Stack{
+    Node* head;
+    Node* top;
+    
+     
+};
+
+int main()
+{
+
+    return 0;
+}
