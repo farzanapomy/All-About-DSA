544d905cdf228b0e8740572092e713b01a1108ea
farzanapomy <farzanapomy56@gmail.com>
2023-01-06 16:28:47 +0600

    fix

--- All about Linked List/stack/stack.cpp
+++ All about Linked List/stack/stack.cpp
@@ -27,18 +27,24 @@ public:
     // PUSH
     void push(int val)
     {
+        // step 1:  create new Node
         Node *newNode = new Node(val);
 
+        // step 2.1: if list is empty then newNode set as a head and top
         if (head == NULL)
         {
             head = top = newNode;
             return;
         }
+
+        // step 2.2: here has must a value so we just add newNode as only top 
         top->next = newNode;
+        newNode->pre = top;
         top = newNode;
     }
 
     // POP
+
     // EMPTY
     // SIZE
     // TOP
